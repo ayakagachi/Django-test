5028fc139a308416e2e78d3bced35bdb6c7ddd25
ayaka <1589337059@qq.com>
2024-03-28 15:25:03 +0800

    message 5:The first iterative development

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -1,11 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page   #(1)
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')    #(2)
         self.assertEqual(found.func,home_page)  #(3)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>',html)
+        self.assertTrue(html.endswith('</html>'))
 
 # class SmokeTest(TestCase):
 

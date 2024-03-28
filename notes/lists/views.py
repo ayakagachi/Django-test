a1c2455385900e5cdc70ecdbfb4105dc6af7ef97
ayaka <1589337059@qq.com>
2024-03-28 23:16:39 +0800

    message 8: Before refactoring the functional_test

--- notes/lists/views.py
+++ notes/lists/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 # home_page = None
 def home_page(request):
-    return render(request,'home.html')
+    # if request.method == 'POST':
+    #     return HttpResponse(request.POST['item_text'])
+    return render(request,'home.html',{'new_item_text': request.POST.get('item_text','')})
 
 # Create your views here.

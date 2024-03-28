b76a6ce66dfcfaacfbee7e7b54456ef694ac8784
ayaka <1589337059@qq.com>
2024-03-28 22:36:20 +0800

    message 7: Refactoring

--- notes/lists/tests.py
+++ notes/lists/tests.py
@@ -5,17 +5,21 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')    #(2)
-        self.assertEqual(found.func,home_page)  #(3)
+    # def test_root_url_resolves_to_home_page_view(self):
+    #     found = resolve('/')    #(2)
+    #     self.assertEqual(found.func,home_page)  #(3)
 
-    def test_home_page_return_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+    # def test_home_page_return_correct_html(self):
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #     html = response.content.decode('utf8')
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>',html)
+    #     self.assertTrue(html.endswith('</html>'))
+        
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
 
 # class SmokeTest(TestCase):
 
--- notes/lists/views.py
+++ notes/lists/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # home_page = None
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request,'home.html')
 
 # Create your views here.

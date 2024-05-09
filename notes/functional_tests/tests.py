060e941a5a0990601d03e9be4ca562dc68b69d24
ayaka <1589337059@qq.com>
2024-05-09 15:11:37 +0800

    message 20: testing for layout + styling

--- notes/functional_tests/tests.py
+++ notes/functional_tests/tests.py
@@ -131,3 +131,17 @@ class NewVisitorTest(LiveServerTestCase):
 
 
 
+
+    def test_layout_and_styling(self):
+        #张三访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #他看到输入框完美的居中显示
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] /2,
+            512,
+            delta=10
+        )
+
